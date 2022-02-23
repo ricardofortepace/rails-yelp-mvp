c9e64603c5906bc92efa12cde6f8a1d2d46e3016
Ricardo Forte Pace <tatopace@hotmail.com>
2022-02-23 14:42:15 -0300

    Finshing the 02

---
+++ db/migrate/20220223144022_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+
+    create_table :reviews do |t|
+      t.text :content
+      t.integer :rating
+
+      t.references :restaurant, foreign_key: true
+
+      t.timestamps
+
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Deleting all old restaurants"
+
+Restaurant.destroy_all
+
+puts "Creating all new restaurants"
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: categories.sample)
+end
